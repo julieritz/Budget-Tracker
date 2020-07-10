551b5a1279be0837290402aeed96a345654a752f
julieritz <julie.ritz@gmail.com>
2020-07-10 10:38:55 -0700

    small changes

---
+++ public/service-worker.js
@@ -0,0 +1,55 @@
+const cacheUrls = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+var CACHE_NAME = "site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Files pre-cached!");
+      return cache.addAll(cacheUrls);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
